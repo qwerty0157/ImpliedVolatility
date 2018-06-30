9f2782d488a97c27fdd204cf58365c38d2a985de
quasar <quasar@MacBook-Air.local>
2018-06-30 19:55:29 +0900

    minor change

--- BlackSholes/Program.cs
+++ BlackSholes/Program.cs
@@ -13,7 +13,7 @@ namespace BlackSholes
             double sigma = 0.1655;
             double rate = 0.00075;
             double initialIV = 0.15;
-            double price = 0.0;
+            double price = 0.1;
             //var Bs = new BlackSholes(110.0, 105, 1, 0, 0.05, 0.001);
 
             var Bs = new BlackSholes(stock, strike, maturity, start, sigma, rate);
