8b8a7f1dc5676a9ef232151003ffdd1a47bb9d51
qwerty0157 <qwerty0157@gmail.com>
2019-02-17 02:35:53 +0900

    add fly adjustment logic

---
+++ FLY調整/為替オプションデータからボラティリティ・スマイルを構築 – Momentum_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"1327 http:\/\/nekopuni.holy.jp\/?p=1327","comments":0}]});
+}
